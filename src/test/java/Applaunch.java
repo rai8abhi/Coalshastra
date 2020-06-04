1cc19e114203e5380b16981e82b5c7d4ffa95586
Abhishek Pandey <abhishekpandey.ext1@grofers.com>
2020-06-04 13:58:38 +0530

    Add comment

--- src/test/java/Applaunch.java
+++ src/test/java/Applaunch.java
@@ -8,16 +8,16 @@ public class Applaunch {
 
     public static AndroidDriver driver;
 
-    @Before
+    @Before     //In this method we are setting Appium Capabilities for the devices on which we are going to run our test cases and also app path.
     public void openApp() throws Exception {
         // Set desired capabilities.
         DesiredCapabilities caps = new DesiredCapabilities();
 
         caps.setCapability("deviceName", "emulator-5554");
-        caps.setCapability("udid", "a87d0588"); //Give Device ID of your mobile phone
+        caps.setCapability("udid", "a87d0588"); //Give Device ID of your test device on test need to be run
         caps.setCapability("platformName", "Android");
-        caps.setCapability("platformVersion", "9.0");
-        caps.setCapability("app", "/Users/grofers/Downloads/app-debug.apk");//res
+        caps.setCapability("platformVersion", "9.0");   //Android version for devices
+        caps.setCapability("app", "/Users/grofers/Downloads/app-debug.apk");// Replace this path with your app location path in system
         // Open the app.
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
     }
--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -7,78 +7,78 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
-public class TestCalass extends Applaunch {
+public class TestClass extends Applaunch {
 
 
-    @Test
+    @Test     //This test case check for valid login test, when user enter correct user id and password.
     public void LoginTest1() {
         try {
-            WebDriverWait wait = new WebDriverWait(driver, 30);
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("emailtv")));
+            WebDriverWait wait = new WebDriverWait(driver, 30);     // Assign wait time 30 seconds
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("emailtv")));    //Used wait to check login page appear before search of the elements.
             MobileElement el1 = (MobileElement) driver.findElementById("emailtv");
-            el1.sendKeys("rai8abhishek@gmail.com");
+            el1.sendKeys("rai8abhishek@gmail.com");     //send login emailID
             MobileElement el2 = (MobileElement) driver.findElementById("passtv");
-            el2.sendKeys("P@ssword001");
+            el2.sendKeys("P@ssword001");        //Send password
             MobileElement el3 = (MobileElement) driver.findElementById("loginll");
             el3.click();
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("dialogeskip")));
             MobileElement el4 = (MobileElement) driver.findElementById("dialogeskip");
             el4.click();
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("largeLabel")));
-            Assert.assertEquals("Login Fail", "Home", driver.findElementById("largeLabel").getText());
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("largeLabel")));     //Wait for home button visible after login.
+            Assert.assertEquals("Login Fail as user is unable to login", "Home", driver.findElementById("largeLabel").getText());      //Verified the login case
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    @Test
+    @Test         // Here we are verifying if we are able to login without password
     public void LoginTest2() {
         try {
-            WebDriverWait wait = new WebDriverWait(driver, 30);
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("emailtv")));
+            WebDriverWait wait = new WebDriverWait(driver, 30);     // Assign wait time 30 seconds
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("emailtv")));        //Used wait to check login page appear before search of the elements.
             MobileElement el1 = (MobileElement) driver.findElementById("emailtv");
-            el1.sendKeys("rai8abhishek@gmail.com");
+            el1.sendKeys("rai8abhishek@gmail.com");     //send login emailID
             MobileElement el2 = (MobileElement) driver.findElementById("passtv");
-            el2.sendKeys("");
+            el2.sendKeys("");           //didn't send the value for password
             MobileElement el3 = (MobileElement) driver.findElementById("loginll");
             el3.click();
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("dialogeskip")));
             MobileElement el4 = (MobileElement) driver.findElementById("dialogeskip");
             el4.click();
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("largeLabel")));
-            Assert.assertNotEquals("Login Fail", "Home", driver.findElementById("largeLabel").getText());
+            Assert.assertNotEquals("User is able to login without password", "Home", driver.findElementById("largeLabel").getText());       //Verifying in this case home should not appear.
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
-    @Test
+    @Test           //Here we are verifying if we are able to login without emailID
     public void LoginTest3() {
         try {
-            WebDriverWait wait = new WebDriverWait(driver, 30);
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("emailtv")));
+            WebDriverWait wait = new WebDriverWait(driver, 30);     // Assign wait time 30 seconds
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("emailtv")));        //Used wait to check login page appear before search of the elements.
             MobileElement el1 = (MobileElement) driver.findElementById("emailtv");
-            el1.sendKeys("rai8abhishek@gmail");
+            el1.sendKeys("");       //login emailID field left blank.
             MobileElement el2 = (MobileElement) driver.findElementById("passtv");
-            el2.sendKeys("P@ssword001");
+            el2.sendKeys("P@ssword001");        //Send password
             MobileElement el3 = (MobileElement) driver.findElementById("loginll");
             el3.click();
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("dialogeskip")));
             MobileElement el4 = (MobileElement) driver.findElementById("dialogeskip");
             el4.click();
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("largeLabel")));
-            Assert.assertNotEquals("Login Fail", "Home", driver.findElementById("largeLabel").getText());
+            Assert.assertNotEquals("User is able to login without user ID", "Home", driver.findElementById("largeLabel").getText());
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    @Test
+    @Test     //Here we are verifying if already register user try to re-register him self, he should not.
     public void Registration() {
         try {
-            WebDriverWait wait = new WebDriverWait(driver, 60);
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("signuptvb")));
+            WebDriverWait wait = new WebDriverWait(driver, 60);     // Assign wait time 60 seconds
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("signuptvb")));      //Used wait to check if registration page appear before search of the elements.
             MobileElement el1 = (MobileElement) driver.findElementById("signuptvb");
             el1.click();
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("nameet")));
@@ -98,7 +98,8 @@ public class TestCalass extends Applaunch {
             el8.sendKeys("P@ssword001");
             MobileElement el9 = (MobileElement) driver.findElementById("signupll");
             el9.click();
-            Assert.assertNotEquals("Login Fail", "Confirm Password *", driver.findElementById("confirmet").getText());
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("confirmet")));
+            Assert.assertEquals("Login Fail", "P@ssword001", driver.findElementById("confirmet").getText());        //Verifying if entered password is same for confirm password.
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
