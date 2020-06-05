1c2e7d9889557189484a855f2f642292e57dc6df
Abhishek Pandey <abhishekpandey.ext1@grofers.com>
2020-06-05 17:30:39 +0530

    Added test support

--- target/cucumber-html-report/report.js
+++ target/cucumber-html-report/report.js
@@ -1,4 +1,101 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/resources/coalShastra/registration.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/resources/coalShastra/login.feature");
+formatter.feature({
+  "name": "feature to test login flow functionality",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "name": "Verify if user is able to login",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "user is on login page",
+  "keyword": "Given "
+});
+formatter.step({
+  "name": "user enter the \u003cemailId\u003e and \u003cpassword\u003e",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "user click on submit button",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "user navigate to login page",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "emailId",
+        "password"
+      ]
+    },
+    {
+      "cells": [
+        "rai8abhishek@gmail.com",
+        "P@ssword001"
+      ]
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Verify if user is able to login",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user is on login page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "StepDefinitions.MyStepdefs.userIsOnLoginPage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user enter the rai8abhishek@gmail.com and P@ssword001",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinitions.MyStepdefs.userEnterTheEmailIdAndPassword(java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user click on submit button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinitions.MyStepdefs.userClickOnSubmitButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user navigate to login page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinitions.MyStepdefs.userNavigateToLoginPage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.uri("file:src/test/java/resources/coalShastra/registration.feature");
 formatter.feature({
   "name": "This is used to verify registration page",
   "description": "",
--- target/test-classes/coalShastra/login.feature
+++ target/test-classes/coalShastra/login.feature
@@ -1,14 +1,14 @@
-#Feature: feature to test login flow functionality
-#
-#  Scenario Outline: Verify if user is able to login
-#    Given user is on login page
-#    When user enter the <emailId> and <password>
-#    And user click on submit button
-#    Then user navigate to login page
-#    Examples:
-#      | emailId                | password    |
-#      | rai8abhishek@gmail.com | P@ssword001 |
-#
+Feature: feature to test login flow functionality
+
+  Scenario Outline: Verify if user is able to login
+    Given user is on login page
+    When user enter the <emailId> and <password>
+    And user click on submit button
+    Then user navigate to login page
+    Examples:
+      | emailId                | password    |
+      | rai8abhishek@gmail.com | P@ssword001 |
+
 #  Scenario Outline: Verify that user should not be able to login, if password field is black
 #    Given user is on login page
 #    When user enter the <emailId> and <password>
